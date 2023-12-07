054055715faa1be56848b7cf97cff98ef98bedb9
Davi Campos <126365210+DaviOCampos@users.noreply.github.com>
2023-12-07 11:12:41 -0300

    remoção de linha

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,5 +1,3 @@
-/// <reference types="Cypress" />
-
 describe('Central de Atendimento ao Cliente TAT', function () {
     it('verifica o título da aplicação', function () {
         cy.visit('./src/index.html')
